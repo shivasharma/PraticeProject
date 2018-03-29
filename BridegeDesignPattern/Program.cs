636f3abe3c10d8910f138cccf4e596d0a7445530
shiva sharma <shivathebravo@hotmail.com>
2018-03-29 16:24:40 -0400

    First Commit

---
+++ BridegeDesignPattern/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridegeDesignPattern
+{
+    public interface IMessageSender
+    {
+        void SendMessage(string subject, string body);
+    }
+
+    public abstract class Message
+    {
+        public string Body { get; set; }
+        public IMessageSender MessageSender { get; set; }
+        public string Subject { get; set; }
+
+        public abstract void Send();
+    }
+
+    public class SystemMessage : Message
+    {
+        public override void Send()
+        {
+            MessageSender.SendMessage(Subject, Body);
+        }
+    }
+
+    public class TextSender : IMessageSender
+    {
+        public void SendMessage(string subject, string body)
+        {
+            string messageType = "Text";
+            Console.WriteLine($"{messageType}");
+            Console.WriteLine("----------------");
+            Console.WriteLine($"Subject{subject} from {messageType}]\nBody {body}");
+        }
+    }
+
+    public class WebserviceSender : IMessageSender
+    {
+        public void SendMessage(string subject, string body)
+        {
+            string messageType = "Web Service";
+            Console.WriteLine($"{messageType}");
+            Console.WriteLine("----------------");
+            Console.WriteLine($"Subject{subject} from {messageType}]\nBody {body}");
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            IMessageSender text = new TextSender();
+            IMessageSender web = new WebserviceSender();
+
+            Message message = new SystemMessage();
+            message.Subject = "A message";
+            message.Body = "Hi there, please accept this message";
+
+            message.MessageSender = text;
+            message.Send();
+
+            message.MessageSender = web;
+            message.Send();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProtoTypeDesignPattern/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProtoTypeDesignPattern
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            ColorController colorController = new ColorController();
+            colorController["yellow"] = new Color(255, 255, 0);
+            colorController["orange"] = new Color(255, 255, 0);
+            colorController["purple"] = new Color(255, 255, 0);
+
+            //user adds personalized color
+            colorController["sunny"] = new Color(255, 123, 0);
+            colorController["tasty"] = new Color(255, 123, 0);
+            colorController["rainy"] = new Color(255, 123, 0);
+
+            //User clones selected colors
+            Color c1 = colorController["yellow"].Clone() as Color;
+
+            Console.ReadKey();
+        }
+    }
+
+    internal class Color : ColorProtoType
+    {
+        private int _orange;
+        private int _purple;
+        private int _yellow;
+
+        public Color(int yellow, int orange, int purple)
+        {
+            _yellow = yellow;
+            _orange = orange;
+            _purple = purple;
+        }
+
+        //crate a shallow copy
+        public override ColorProtoType Clone()
+        {
+            Console.WriteLine("RGB color is cloned to{0,3},{1,3},{2,3}", _yellow, _orange, _purple);
+
+            return MemberwiseClone() as ColorProtoType;
+        }
+    }
+
+    internal class ColorController
+    {
+        private Dictionary<string, ColorProtoType> _colors = new Dictionary<string, ColorProtoType>();
+
+        public ColorProtoType this[string key]
+        {
+            get { return _colors[key]; }
+            set { _colors.Add(key, value); }
+        }
+    }
+
+    internal abstract class ColorProtoType
+    {
+        public abstract ColorProtoType Clone();
+    }
+}
